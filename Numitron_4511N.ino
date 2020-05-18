bd0d8e7475fa6da8e28b2e3a0d925757dc0f14c9
gpeter62 <34945081+gpeter62@users.noreply.github.com>
2020-05-18 15:57:25 +0200

    Add files via upload

--- Numitron_4511N.ino
+++ Numitron_4511N.ino
@@ -1,6 +1,8 @@
 #ifdef Numitron_4511N
 //used by GP Numitron v3 panel
 
+byte panelVersion = 3;   //1,2 or 3
+
 #define LTBIpin 5
 byte digitEnablePins[] = {13,12,14,16};    //define here the digit enable pins from 4 to 8
 byte ABCDPins[4] = {4,0,2,15};
@@ -22,15 +24,19 @@ void setup_pins() {
 void ICACHE_RAM_ATTR writeDisplay(){        //https://circuits4you.com/2018/01/02/esp8266-timer-ticker-example/
 static volatile int brightCounter = 1;
 byte num;
-  
-  for (int pos=0; pos<=maxDigits; pos++) {
-    num = digit[pos]; 
-    if ((displayON ?  prm.dayBright : prm.nightBright)<brightCounter)  num = 10;
-    digitalWrite(digitEnablePins[pos],LOW);   
-    for (int j=0;j<4;j++) {digitalWrite(ABCDPins[j],num  & (1<<j)); }
-    digitalWrite(digitEnablePins[pos],HIGH);    
+  if ((panelVersion==3) && ((displayON ?  prm.dayBright : prm.nightBright)<brightCounter)) {
+    digitalWrite(LTBIpin,LOW);
   }
-  
+  else { 
+    digitalWrite(LTBIpin,HIGH);
+    for (int pos=0; pos<=maxDigits; pos++) {
+      num = digit[pos]; 
+      if ((displayON ?  prm.dayBright : prm.nightBright)<brightCounter)  num = 10;   //clear digit
+      digitalWrite(digitEnablePins[pos],LOW);   
+      for (int j=0;j<4;j++) {digitalWrite(ABCDPins[j],num  & (1<<j)); }
+      digitalWrite(digitEnablePins[pos],HIGH);    
+    } //end for
+  } //end else
   brightCounter++; 
   if (brightCounter>MAXBRIGHTNESS) brightCounter = 1;
   
